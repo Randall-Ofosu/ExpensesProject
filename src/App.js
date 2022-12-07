0b1fb6549c6212c69dc707d5fad95aef522ebc38
Randall-Ofosu <obrempongamoakorandallofosu@gmail.com>
2022-12-07 02:42:49 -0800

    Final

--- src/App.js
+++ src/App.js
@@ -35,8 +35,7 @@ const App = () => {
   return (
     <div>
     <h2>Let's get started!</h2>
-    <h2>Let's get started!</h2>
-    <h2>Let's get started!</h2>
+ 
 
     <NewExpense onAddExpense={addExpenseHandler} />
     <Expense items={expenses}/>
